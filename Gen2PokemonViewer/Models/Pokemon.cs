000de11164dee41a284b4c37fc93c9d907ab6de8
Mike <homerun32295@gmail.com>
2020-06-04 14:32:00 -0400

    Initial Commit

---
+++ Gen2PokemonViewer/Models/Pokemon.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Gen2PokemonViewer.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public object[] Abilities { get; set; }
+        public object[] Forms { get; set; }
+        public int Height { get; set; }
+        public int InternalId { get; set; }
+        [JsonProperty("is_default")]
+        public bool IsDefault { get; set; }
+
+        public object[] Species { get; set; }
+        public Dictionary<string, string> Sprites { get; set; }
+
+        public object[] Types { get; set; }
+        public int Weight { get; set; }
+    }
+
+    
+}
